405b8de928bbee73e214ea82addc8fd63c8bc833
aabir <shblnmn@gmail.com>
2023-05-04 21:39:55 +0800

    Font awesome package installed.

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,24 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import { getPortfolioStaticProps } from './api/staticProps'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faGift } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home({portfolio}) {
+
+  function getIconForNetwork(network) {
+    switch (network.toLowerCase()) {
+      case "GitHub":
+        return faGithub;
+      case "LinkedIn":
+        return faLinkedin;
+      case "gitconnected":
+        return faGift;
+      default:
+        return null;
+    }
+  }
+
   return (
     <Layout home portfolio={portfolio}>
       <Head>
@@ -12,17 +28,25 @@ export default function Home({portfolio}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-         {portfolio.basics.summary}
+          {portfolio.basics.summary}
         </p>
-        <ul className='socialConnect'>
-        {portfolio.basics.profiles.map((profile, index) => (
-          <li key={index}>{profile.network}</li>
-        ))}
+        <ul className="socialConnect">
+          {portfolio.basics.profiles.map((profile, index) => {
+            if(index === 0 ) return null;
+            const icon = getIconForNetwork(profile.network);
+            return (
+              <li key={index}>
+                <a href={profile.url} target="_blank" rel="noopener noreferrer">
+                  {icon && <FontAwesomeIcon icon={icon} />} {profile.network}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </section>
+      <FontAwesomeIcon icon={faLinkedin} />
       <Link href="/cv"> CV </Link>
     </Layout>
-
   )
 }
 
