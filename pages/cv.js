0832abb6623bcb95017ecad5d1053c03a65fd214
aabir <shblnmn@gmail.com>
2023-05-21 01:29:21 +0800

    Header avater and title added.

--- pages/cv.js
+++ pages/cv.js
@@ -3,15 +3,36 @@ import { getPortfolioStaticProps } from './api/staticProps'
 import Date from '../components/date'
 import styles from '../styles/utils.module.css'
 import Layout, { siteTitle } from '../components/layout'
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
+import Image from 'next/image'
 
 const Cv = ({ portfolio }) => {
 
   function formatSummary(summary) {
-    return summary.replace(/\n/g, '\n - ');
+    return summary.replace(/\n/g, '\n');
   }
 
   return (
     <div className={styles.container}>
+      <head>
+        <title>Shible Noman's CV</title>
+      </head>
+      <h2>
+        <Link href="/">SNA</Link>
+      </h2>
+      <div className='avater'>
+        <Image
+              priority
+              src={portfolio?.basics?.image || "/images/profile.jpg"}
+              className={utilStyles.borderCircle}
+              height={75}
+              width={75}
+              alt={portfolio.basics.name}
+        /> 
+        <div className='headerName'>{portfolio.basics.name}</div>
+        <div className='headerPosition'>{portfolio.basics.label}</div>
+      </div>
       <h4>Work Experience</h4>
         <ul>
           {portfolio.work.map((job, index) => (
