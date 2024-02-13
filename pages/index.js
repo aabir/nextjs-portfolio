9dbd47dcce9f46a881fab25023f9a49cdfaa76fb
Shible Chowdhury <shible.chowdhury@theaccessgroup.com>
2024-02-14 00:52:07 +0800

    Icon package updated.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import { getPortfolioStaticProps } from './api/staticProps'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin, faGift } from '@fortawesome/free-solid-svg-icons'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import { getPortfolioStaticProps } from './api/staticProps';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// Import brand icons from free-brands-svg-icons
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+// Keep faGift from free-solid-svg-icons for non-brand icons
+import { faGift } from '@fortawesome/free-solid-svg-icons';
 
-export default function Home({portfolio}) {
+export default function Home({ portfolio }) {
 
   function getIconForNetwork(network) {
     switch (network.toLowerCase()) {
-      case "GitHub":
+      case "github":
         return faGithub;
-      case "LinkedIn":
+      case "linkedin":
         return faLinkedin;
       case "gitconnected":
         return faGift;
@@ -32,19 +35,18 @@ export default function Home({portfolio}) {
         </p>
         <ul className="socialConnect">
           {portfolio.basics.profiles.map((profile, index) => {
-            if(index === 0 ) return null;
+            if (index === 0) return null; // Assuming you want to skip the first profile for some reason
             const icon = getIconForNetwork(profile.network);
             return (
               <li key={index}>
                 <a href={profile.url} target="_blank" rel="noopener noreferrer">
-                  {icon && <FontAwesomeIcon icon={icon} />} {profile.network}
+                  {icon && <FontAwesomeIcon icon={icon} />} <span className="sr-only">{profile.network}</span>
                 </a>
               </li>
             );
           })}
         </ul>
       </section>
-      <FontAwesomeIcon icon={faLinkedin} />
       <Link href="/cv"> CV </Link>
     </Layout>
   )
