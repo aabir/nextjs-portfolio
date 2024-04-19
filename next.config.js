489898862826e71b0e04c01183f38ad68c0cefb5
Shible Chowdhury <shible.chowdhury@theaccessgroup.com>
2024-04-19 17:56:56 +0800

    update config file.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['avatars2.githubusercontent.com'],
+  },
   output:'export',
 };
 export default nextConfig;
