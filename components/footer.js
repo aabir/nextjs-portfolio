c8a8961eaf2daee06423ed50d51a16c0827fb0e8
Shible Chowdhury <shible.chowdhury@theaccessgroup.com>
2024-07-23 07:59:19 +0800

    Files updated.

--- components/footer.js
+++ components/footer.js
@@ -5,14 +5,15 @@ const Footer = () => (
         <p>Built using Next.js and with gitconnected.com API </p>
         <style jsx>{`
             footer {
-                margin-top: 0; /* take available whitespace */
-                margin-bottom: -10px;      /* remove bottom whitespace */
+                margin-top: 0;
+                margin-bottom: -10px;
                 padding-bottom: 5px;
-                height: 100px; 
+                height: 100px;
                 margin: 0 auto;
             }
             p {
                 margin: 0;
+                font-size: 0.9em;
             }
         `}</style>
     </footer>
