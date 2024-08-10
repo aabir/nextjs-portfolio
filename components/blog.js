f1139bfb1b4d5e83ccc47a4924aa45497386a43f
aabir <shblnmn@gmail.com>
2024-08-10 00:07:04 -0700

    created constant url

--- components/blog.js
+++ components/blog.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { getPosts } from '../pages/api/api';
 import utilStyles from '../styles/utils.module.css';
+import { BLOG_URL } from './Constants';
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
@@ -14,25 +15,32 @@ const Blog = () => {
     fetchPosts();
   }, []);
 
+  const transformLink = (originalLink) => {
+    const url = new URL(originalLink);
+    const pathParts = url.pathname.split('/').filter(Boolean);
+    const blogTitle = pathParts[pathParts.length - 1];
+    return `${BLOG_URL}posts/${blogTitle}`;
+  };
+
   return (
     <section>
-      <h2>Learning Notes</h2>
+      <h2>Development Notes</h2>
       <hr />
       {posts.map((post) => (
         <h3 key={post.id} className={utilStyles.blogTitle}>
-          <a href={post.link} target='_blank'>{post.title.rendered}</a>
+          <a href={transformLink(post.link)} target='_blank'>{post.title.rendered}</a>
           <span className={utilStyles.blogDate}>
-              {new Date(post.date).toLocaleString('en-GB', {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric',
-              })}
-            </span>
+            {new Date(post.date).toLocaleString('en-GB', {
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric',
+            })}
+          </span>
         </h3>
       ))}
 
-      <div style={{textAlign: 'center', color: '#b2bac2'}}>
-        <a href='https://shiblenoman.com/blog/' target='_blank'>More posts</a>
+      <div style={{ textAlign: 'center', color: '#b2bac2' }}>
+        <a href={BLOG_URL} target='_blank'>More posts</a>
       </div>
     </section>
   );
