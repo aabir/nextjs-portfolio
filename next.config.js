945c6adb5f283d63e98bc523d28ecab6e6863903
Aabir <shblnmn@gmail.com>
2026-01-11 08:57:21 -0800

    proxy added.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = 
+  {async rewrites() {
+    return [
+      {
+        // This is the path you will use in your frontend code
+        source: '/api/blog/:path*',
+        // This is where the request is actually sent (your WordPress API)
+        destination: 'https://aabirtech.com/shiblenomanblog/wp-json/wp/v2/:path*',
+      },
+    ]
+  },
   images: {
     domains: ['avatars2.githubusercontent.com'],
   },
